aba858df9cd7efdc78f896a98fee9fa6be254d14
yashika-yadav123 <yadavyashika0100@gmail.com>
2024-07-05 23:26:03 +0530

    navbar-perk-prize

---
+++ src/constants/index.ts
@@ -0,0 +1,51 @@
+
+import reward from "../assets/reward.png"
+import network from "../assets/network.png"
+import gain from "../assets/gain.png"
+import collaborate from "../assets/collaborate.png"
+
+export const nav = [
+    {
+        name: "Home",
+        link: "/"
+    },
+    {
+        name: "About Us",
+        link: "/#about-us"
+    },
+    {
+        name: "Team",
+        link: "/#team"
+    },
+    {
+        name: "Timelines",
+        link: "/#timelines"
+    },
+    {
+        name: "FAQ",
+        link: "/#faq"
+    }
+];
+
+export const sections = [
+    {
+        imgSrc: reward,
+        title: "Rewards",
+        description: "Experience the thrill of victory with a tantalizing Rs. ₹ CASH PRIZE pool awaiting our champions!"
+    },
+    {
+        imgSrc: network,
+        title: "Expand network",
+        description: "Connect with industry professionals and recruiters and other teams to learn and grow more."
+    },
+    {
+        imgSrc: gain,
+        title: "Additional gains",
+        description: "Cool and awesome swags for every one and special gifts for some lucky participants!"
+    },
+    {
+        imgSrc: collaborate,
+        title: "Collaborate and skill up",
+        description: "Connect with people, form a team, learn new skills and develop amazing projects by using uAgent!"
+    }
+];
